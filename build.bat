35b019928a6406202e60f261d2c2c947ac07cc16
Staffan Thomen <duck@shangtai.net>
2017-06-25 13:58:05 +0300

    Update build.bat to make a proper jar with package

--- build.bat
+++ build.bat
@@ -1,14 +1,16 @@
 @echo off
 set JAVAC=%JAVA_HOME%\bin\javac.exe
 set JAR=%JAVA_HOME%\bin\jar.exe
+set SOURCES=src
+set BASEPACKAGE=net
 
 for /f %%f in ('dir /b snmp4j*.jar') do set "SNMP4J=%%~nxf"
 
-set JAVAC_ARGS=-source "1.7" -target "1.7" -sourcepath src -d . -cp ".;%SNMP4J%"
+set JAVAC_ARGS=-source "1.7" -target "1.7" -d . -cp ".;%SNMP4J%"
 
 echo Compiling java files...
 
-"%JAVAC%" %JAVAC_ARGS% src\SnmpHelper.java
+"%JAVAC%" %JAVAC_ARGS% %SOURCES%\*.java
 
 echo Create MANIFEST...
 
@@ -16,11 +18,11 @@ echo Class-Path: %SNMP4J% > MANIFEST
 
 echo Packaging JAR...
 
-"%JAR%" cvfm SnmpHelper.jar MANIFEST *.class
+"%JAR%" cvfm SnmpHelper.jar MANIFEST %BASEPACKAGE%
 
 echo Cleaning up...
 
 del MANIFEST
-del *.class
+rmdir /Q /S %BASEPACKAGE%
 
 echo All done.
